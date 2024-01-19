e15ff6aaabd5bf31c4bcd05a343dd87cd4b7a178
leminhquocbao <156556372+leminhquocbao@users.noreply.github.com>
2024-01-19 17:45:49 +0700

    Hello
    fdsdfsdf

---
+++ Hello/src/til/edu/xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package til.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello word");
+	}
+
+}
